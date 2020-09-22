3d0625dae79a430b346bf61872764360b463109e
Sebastiaan Verbeek <sebastiaanverbeek@Sebastiaans-MacBook-Air.local>
2020-09-22 13:47:20 +0700

    fix: just gonna work with css grid

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createElement } from './initialPageLoad';
+import "./style.css";
 
 const content = document.querySelector("#content");
 content.appendChild(createElement("h1", "Vietnamese Restaurant Page"));
