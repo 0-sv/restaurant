cd11a43844fb3eeb74dd42fc444fdaa45ce37faa
Sebastiaan Verbeek <sebastiaanverbeek@Sebastiaans-MacBook-Air.local>
2020-09-23 10:16:47 +0700

    fix: removed code temporary while setting up html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import { createElement } from './initialPageLoad';
 import "./style.css";
-
-const content = document.querySelector("#content");
-content.appendChild(createElement("h1", "Vietnamese Restaurant Page"));
